43e2c5e159fa4fe55cc215371f576511f08855e6
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-05-17 23:42:53 +0200

    Add theme file

---
+++ src/files/themes/dark.tsx
@@ -0,0 +1,10 @@
+import { theme, type ThemeConfig } from "antd";
+
+const darkTheme: ThemeConfig = {
+  algorithm: theme.darkAlgorithm,
+  token: {
+    fontSize: 16,
+  },
+};
+
+export default darkTheme;
---
+++ src/files/themes/light.tsx
@@ -0,0 +1,10 @@
+import { theme, type ThemeConfig } from "antd";
+
+const lightTheme: ThemeConfig = {
+  algorithm: theme.defaultAlgorithm,
+  token: {
+    fontSize: 16,
+  },
+};
+
+export default lightTheme;
